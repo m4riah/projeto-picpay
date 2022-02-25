3e7143682168660dadbf9cd03b8962c695c05d9a
Maria Eduarda <mariaeduardamend3s@gmail.com>
2022-02-25 20:47:33 -0300

    andamento do projeto picpay

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,32 @@
+// selecionando UL(lista)
 const listaOpcoe = document.querySelector(".secaoDetalhesConta_opcoesTransacoes")
 
+// adicionando escutador de eventos
 listaOpcoe.addEventListener("click", indentificarOpcoes)
 
+// executando ação depois do clique
 function indentificarOpcoes(event){
 
+    // identificando onde ocorreu o clique
     const elemento = event.target 
 
+    // verificando se é um LI
     if(event.target.tagName == "LI"){
 
+        // recuperando o id
         const id = elemento.id
-         console.log(id)
+
+        //  selecionando uma seção utilizando o ID (escolha do usuário)
         const secaoEscolha = document.querySelector(`div[data-id="${id}"]`)
 
+        // removendo classe mostrar
        removeClasseMostrar()
 
+    //    adicionar uma classe
        secaoEscolha.classList.add("mostrar")
     }
 }
-
+// função para remover as classes das divs
 function removeClasseMostrar(){
     const divs = document.querySelectorAll(".secaoTransacao .container div")
     
